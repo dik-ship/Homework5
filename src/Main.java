1a3ec16e949fb682d3987990e8c48cc98f949979
Dasha <dasadik@Mac.Home>
2025-11-21 22:17:52 +0300

    Проверьте дз 5

--- src/Main.java
+++ src/Main.java
@@ -76,7 +76,7 @@ public class Main {
                 System.out.println("Зима");
                 break;
             default:
-                System.out.println("Такого месяца не существует");
+                System.out.println("Такого месяца не существует ");
         }
     }
 }
